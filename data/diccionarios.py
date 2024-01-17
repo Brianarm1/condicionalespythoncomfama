3641c782e9a17bfff1e77a4418431b5eba85b263
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-16 20:22:29 -0500

    NT2 se crean ejemplos de listas, diccionarios y ciclos

---
+++ data/diccionarios.py
@@ -0,0 +1,15 @@
+
+
+#Declarando diccionario en python - Vamos a utilizar un conjunto de datos / Se puede comparar con un archivo Json de JavaScript
+
+arbol={
+
+    "nombre": "Ceiba africana",
+    "cantidad": 500,
+    "esNativoEnColombia": False
+    
+}
+
+print()
+
+
---
+++ data/listas.py
@@ -0,0 +1,44 @@
+
+#Toda lista se debe escribir en PLURAL
+
+arboles=["Ceiba","Yarumo","Manzano","Guayacan"]
+
+
+municipios=["Medelin","titiribi","Carolina del principe"]
+
+hecareasSembradas=[2500,500,1200]
+
+lluviasMayoresA500=[False,True,True]
+
+#Recorriendo un arreglo.
+#Acceder de FORMA DINAMICA al conitendo de un arreglo.
+#Para recorrerlo debo utilizar un ciclo o bucle o loop.
+
+
+#Ciclo while (Mientras)
+
+'''
+contador=0
+while contador<10 :
+    print("Estoy triunfado......")
+    contador=contador+1 '''
+
+#Ciclo For (Para)
+
+''' for i in range(0,301,2):
+ print(i) '''
+
+#Recorrer dinamicamente un arreglo usando un FOREACH (PARA CADA UNO)
+
+for arbol in arboles:
+    print(arbol)
+ 
+print("-------------------")
+
+for municipio in municipios:
+    print(municipio)
+
+
+ 
+ 
+ 
